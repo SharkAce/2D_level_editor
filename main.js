3e5b4f6ecc3d760ac750f3a1a73ecc3be3a408a4
SharkAce <charles.marchildon2@gmail.com>
2022-09-14 10:33:07 -0400

    update

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 let spriteSheetURL;
 let spriteSheet;
-let spriteArray = [];
-let drawSpriteArray = [];
+let spriteSize;
 let levelSize = {x:0,y:0};
 let selected = 1;
 let levelArray = [];
-let tileSize;
-const tileSelectorWidth = 400;
+
+let drawSpace = {
+    tileSize: null,
+    spriteArray: []
+};
+
+let selector = {
+    width: 400,
+    tileSize: 25,
+    spriteArray: []
+};
 
 const s = ( p ) => {
 
-    let x = 100; 
-    let y = 100;
-  
     p.setup = function() {
       p.createCanvas(p.displayWidth, p.displayHeight);
-      spriteArray = loadSpriteArray(spriteSheet,16,255,255);
       
-      tileSize = ((p.displayWidth-tileSelectorWidth-18)/levelSize.x);
+      drawSpace.tileSize = ((p.displayWidth-selector.width-18)/levelSize.x);
 
-      drawSpriteArray = loadSpriteArray(spriteSheet,16,255,255);
-      drawSpriteArray.forEach((tile) => tile.resize(tileSize,tileSize));
+      selector.spriteArray = loadSpriteArray(spriteSheet,selector.tileSize,255,255);
+
+      drawSpace.spriteArray = loadSpriteArray(spriteSheet,drawSpace.tileSize,255,255);
 
     };
   
     p.draw = function() {
       p.background(120);
       p.fill(255);
-      p.rect(0,0,tileSelectorWidth,p.displayHeight);
+      p.rect(0,0,selector.width,p.displayHeight);
       drawSelectTiles(p);
       drawSelectRect(p);
       editLevel(p);
@@ -41,9 +46,9 @@ const s = ( p ) => {
     p.o
 
     p.mouseClicked = function() {
-        if (p.mouseX < tileSelectorWidth){
-            let tileX = Math.floor(p.mouseX / 25);
-            let tileY = Math.floor(p.mouseY / 25);
+        if (p.mouseX < selector.width){
+            let tileX = Math.floor(p.mouseX / selector.tileSize);
+            let tileY = Math.floor(p.mouseY / selector.tileSize);
             selected = tileY*16 + tileX;
     }
   };
@@ -51,8 +56,9 @@ const s = ( p ) => {
 
 function startEditor(){
     spriteSheetURL = loadImageURL("spriteSheet");
-    levelSize.x = document.getElementById("levelX").value;
-    levelSize.y = document.getElementById("levelY").value;
+    levelSize.x = parseInt(document.getElementById("levelX").value);
+    levelSize.y = parseInt(document.getElementById("levelY").value);
+    spriteSize = parseInt(document.getElementById("spriteSize").value);
     initLevelArray();
     let myp5 = new p5(s);
     document.getElementById("initForm").remove();
@@ -68,12 +74,12 @@ function loadImageURL(imageElementId){
 
 function loadSpriteArray(image,tileSize,imageX,imageY){
     let arr = [];
-    for (let i=0; i<imageY; i+=tileSize){
-        for (let j=0; j<imageX; j+=tileSize){
-            arr.push(image.get(j,i,tileSize,tileSize));
+    for (let i=0; i<imageY; i+=spriteSize){
+        for (let j=0; j<imageX; j+=spriteSize){
+            arr.push(image.get(j,i,spriteSize,spriteSize));
         }
     }
-    arr.forEach((tile) => tile.resize(25,25));
+    arr.forEach((tile) => tile.resize(tileSize,tileSize));
     return arr;
 };
 
@@ -90,29 +96,29 @@ function initLevelArray(){
 function drawGrid(p){
 
     for (let i=0; i<=levelSize.y; i++){
-        let y = i*(tileSize)
-        p.line(tileSelectorWidth,y,p.displayWidth,y);
+        let y = i*(drawSpace.tileSize)
+        p.line(selector.width,y,p.displayWidth,y);
     };
     for (let i=0; i<=levelSize.x; i++){
-        let x = (i*(tileSize))+tileSelectorWidth;
+        let x = (i*(drawSpace.tileSize))+selector.width;
         p.line(x,0,x,p.displayHeight);
     };
-    p.rect(tileSelectorWidth,levelSize.y*tileSize,p.displayWidth,p.displayHeight);
+    p.rect(selector.width,levelSize.y*drawSpace.tileSize,p.displayWidth,p.displayHeight);
 };
 
 function drawSelectTiles(p){
     let i=0;
-    for (let y=0;y<(spriteArray.length/16);y++){
+    for (let y=0;y<(selector.spriteArray.length/16);y++){
         for (let x=0;x<16;x++){
-            p.image(spriteArray[i],x*25,y*25);
+            p.image(selector.spriteArray[i],x*selector.tileSize,y*selector.tileSize);
             i++;
         };
     };
 };
 
 function drawSelectRect(p){
-    let y = (Math.floor(selected/16))*25;
-    let x = (selected%16)*25;
+    let y = (Math.floor(selected/16))*selector.tileSize;
+    let x = (selected%16)*selector.tileSize;
     p.noFill();
     p.stroke('blue');
     p.strokeWeight(3);
@@ -123,21 +129,20 @@ function drawSelectRect(p){
 }
 
 function editLevel(p){
-    if (!(p.mouseIsPressed) || (p.mouseX < tileSelectorWidth)) return;
+    if (!(p.mouseIsPressed) || (p.mouseX < selector.width)) return;
     console.log('clicking')
-    let tileX = Math.floor((p.mouseX - tileSelectorWidth) / tileSize);
-    let tileY = Math.floor((p.mouseY) / tileSize);
+    let tileX = Math.floor((p.mouseX - selector.width) / drawSpace.tileSize);
+    let tileY = Math.floor((p.mouseY) / drawSpace.tileSize);
     console.log(tileX, tileY, selected)
     levelArray[tileX][tileY] = selected;
 };
 
 function drawEditedLevel(p){
-    // for (let i=0; i<levelArray.length; i++){
-    //     for (let j=0; j<levelArray[0].lenght; j++){
-    //         if (levelArray[i][j] != -1){
-    //             console.log("debug")
-    //             p.image(drawSpriteArray[(i*16)+j],(i*tileSize)+tileSelectorWidth,j*tileSize);
-    //         }
-    //     }
-    // };
+    for (let i=0; i<levelArray.length; i++){
+        for (let j=0; j<levelArray[0].length; j++){
+            if (levelArray[i][j] != -1){
+                p.image(drawSpace.spriteArray[(levelArray[i][j])],(i*drawSpace.tileSize)+selector.width,j*drawSpace.tileSize);
+            }
+        }
+    };
 };
